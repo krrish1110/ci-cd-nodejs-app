727f62d7770b8c4c1da621dfe57f7ecd71d38e96
krrish1110 <krrish.kj1110@gmail.com>
2022-08-25 19:14:47 +0530

    addded 4 !

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Welcome! To the 1st CI CD Pipeline Testing of NodeJS application'));
+app.get('/', (req, res) => res.send('Welcome! To the 1st CI CD Pipeline Testing of NodeJS application!!!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
